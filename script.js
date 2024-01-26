93418c7565cd610fc7b1244c9303831ce893aaba
erdiozer <ozer.erdi74@gmail.com>
2024-01-27 02:44:13 +0300

    Responsive

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ const movieLists = document.querySelectorAll(".movie-list");
 arrows.forEach((arrow, i) => {
     let clickCounter = 0;
     const imageItem = movieLists[i].querySelectorAll("img").length;
-    
+    const widthRatio = Math.floor(window.innerWidth / 300);
     arrow.addEventListener("click", function () {
-        if (imageItem - (4 + clickCounter) > 0) {
+        if (imageItem - (4 + clickCounter) + (4 - widthRatio) > 0) {
             clickCounter += 1;
             movieLists[i].style.transform = `translateX(${movieLists[i].computedStyleMap().get("transform")[0].x.value - 300 }px)`;
         } else {
@@ -15,7 +15,7 @@ arrows.forEach((arrow, i) => {
             movieLists[i].style.transform = "translateX(0)";
         }
     });
-});
+}); 
 
 
 // dark mode
@@ -24,6 +24,6 @@ const items = document.querySelectorAll(
     ".container, .navbar, .sidebar, .sidebar i,.movie-list-title, .toggle, .toggle-ball, .movie-list-filter select, .arrow");
 
 ball.addEventListener("click", function () {
-    items.forEach((item) => item.classList.toggle("active"));
+    items.forEach((item) => item.classList.toggle("active")); 
     // console.log(items);
 });
